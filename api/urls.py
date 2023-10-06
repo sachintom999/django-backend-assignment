a78fe5be92af2f1ab651171ab9b9d58ee4601d18
Sachin Tom <sachintomacc@gmail.com>
2023-10-06 23:20:04 +0530

    Added jwt auth

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
-urlpatterns = [path("login", views.login), path("signup", views.signup)]
+urlpatterns = [
+    path("login", views.login),
+    path("signup", views.signup),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
