89daeac6db3dbf26e305ceb764c49ff54d018f12
Sachin Tom <sachintomacc@gmail.com>
2023-10-06 16:04:55 +0530

    Added models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
