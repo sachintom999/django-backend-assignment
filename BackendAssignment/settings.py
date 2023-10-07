9f2a9ee52bd788349291f2447173fafe7c8a1f4a
Sachin Tom <sachintomacc@gmail.com>
2023-10-07 12:23:47 +0530

    Adding django secret to env

--- BackendAssignment/settings.py
+++ BackendAssignment/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-^d0)_vl8y=2im-i^-$%8^k$seg)(^eyy8&$*r=-!_c&xk*5j+v"
-)
+SECRET_KEY = config("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
