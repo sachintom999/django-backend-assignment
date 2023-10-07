7e7de38a9bcbdbab7f1e4fa0a83bd7b8b43c4a06
Sachin Tom <sachintomacc@gmail.com>
2023-10-07 09:02:23 +0530

    Adding postgres db to project

--- BackendAssignment/settings.py
+++ BackendAssignment/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,21 +83,14 @@ WSGI_APPLICATION = "BackendAssignment.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'your_database_name',
-#         'USER': 'your_database_user',
-#         'PASSWORD': 'your_database_password',
-#         'HOST': 'localhost',  # or your PostgreSQL server address
-#         'PORT': '5432',       # the default PostgreSQL port
-#     }
-# }
 
 
 # Password validation
